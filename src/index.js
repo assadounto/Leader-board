264e01755cbdbcd2fd9080f940b8ed47fd1acc2e
Rich <adukyerer@gmail.com>
2022-06-15 17:41:58 +0000

    Fix js linters

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-///* eslint-disable linebreak-style */
 import './styles/style.css';
 import Api from './modules/ConsumeApi.js';
-// Game with ID: uBjkXQG2z5rv8yctQctd
 
+const displayData = async () => {
+  const data = await Api.getScores();
+  Api.renderScore(data);
+};
+displayData();
+document.getElementById('form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const form = document.getElementById('form');
+  const name = document.getElementById('name');
+  const score = document.getElementById('score');
+  const data = {
+    user: name.value,
+    score: score.value,
+  };
+  await Api.addScore(data);
+  form.reset();
+});
 
-const displayData = async ()=> {
-    const data = await Api.getScores();
-    Api.renderScore(data)
-}
-displayData()
-document.getElementById('form').addEventListener('submit', async(e)=>{
-    e.preventDefault();
-    //const execute=document.getElementById('form')
-   const name =document.getElementById('name')
-   const score = document.getElementById('score')
-    const data ={
-        user: name.value,
-        score: score.value,
-    }
-    await Api.addScore(data)
-    form.reset()
-})
-
-document.getElementById('refresh').addEventListener('click', async()=>{
-    displayData()
-})
+document.getElementById('refresh').addEventListener('click', async () => {
+  displayData();
+});
--- src/modules/ConsumeApi.js
+++ src/modules/ConsumeApi.js
@@ -1,33 +1,32 @@
-const myUrl= 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/uBjkXQG2z5rv8yctQctd/scores/';
+const myUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/uBjkXQG2z5rv8yctQctd/scores/';
 
-  export default class Api {
-
+export default class Api {
     static getScores= async () => {
-           const { result } = await fetch(myUrl).then(response => response.json())
-           .catch((error)=> new Error(error)
-           );
-           return result
+      const { result } = await fetch(myUrl).then((response) => response.json())
+        .catch((error) => new Error(error));
+      return result;
     }
 
-    static addScore = async (score)=> {
-        await fetch(myUrl, {
-            method: 'POST',
-            body:JSON.stringify(score),
-            headers:{'Content-type': 'application/json; charset=UTF-8'},
-        })
+    static addScore = async (score) => {
+      await fetch(myUrl, {
+        method: 'POST',
+        body: JSON.stringify(score),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      });
     }
+
     static renderScore = async (scores) => {
-            const scoreArea = document.querySelector('.scores');
-            scoreArea.innerHTML='';
-            scores.forEach(element => {
-                const cont = document.createElement('div')
-                cont.classList.add('cont')
-                const name = document.createElement('div');
-                const score = document.createElement('div');
-                name.textContent= element.user;
-                score.textContent=element.score;
-                [name,score].forEach(element=> cont.appendChild(element));
-                scoreArea.appendChild(cont)
-            });
-        }
+      const scoreArea = document.querySelector('.scores');
+      scoreArea.innerHTML = '';
+      scores.forEach((element) => {
+        const cont = document.createElement('div');
+        cont.classList.add('cont');
+        const name = document.createElement('div');
+        const score = document.createElement('div');
+        name.textContent = element.user;
+        score.textContent = element.score;
+        [name, score].forEach((element) => cont.appendChild(element));
+        scoreArea.appendChild(cont);
+      });
     }
+}
