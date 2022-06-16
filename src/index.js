2e927bb78c22de8a89a161162bd0bc04bf3973ab
Rich <adukyerer@gmail.com>
2022-06-16 11:09:19 +0000

    add build 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './styles/style.css';
+import renderScore from './modules/render.js';
 import Api from './modules/ConsumeApi.js';
 
 const displayData = async () => {
   const data = await Api.getScores();
-  Api.renderScore(data);
+  renderScore(data);
 };
 
 displayData();
